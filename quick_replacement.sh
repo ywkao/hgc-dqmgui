ab7c674ebcd9ccf59089f20e442af81d38b26f45
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2024-09-03 09:11:07 +0200

    update script for quick (re)placements of HGCAL rendering plugins

--- quick_replacement.sh
+++ quick_replacement.sh
@@ -1,38 +1,22 @@
 #!/bin/bash
-# script to be executed under current/sw/slc7_amd64_gcc630/cms/dqmgui/
+current="."
+destination="/data/srv/current/config/dqmgui"
+backup="./backup"
 
-dir="9.7.8"
-ori_dir="original_9.7.8"
-new_dir="new_feature_9.7.8.2"
+function update(){
+    f=$1
+    d=$2
 
-function copy()
-{
-    file=$1
-    echo "cp ${new_dir}/${file} ${dir}/${file}"
-    cp ${new_dir}/${file} ${dir}/${file}
+    cp -p ${current}/$f $d
+    ls -lhrt $d/$f
 }
 
-function copy_dir()
-{
-    file=$1
-    echo "cp -r ${new_dir}/${file} ${dir}/${file}"
-    cp -r ${new_dir}/${file} ${dir}/${file}
-}
+# backup workspaces-online.py
+mkdir -p ${backup}
+cp -p "/data/srv/current/config/dqmgui/workspaces-online.py" ${backup}
 
-# backup original directory first
-echo "cp -r $dir $ori_dir"
-cp -r $dir $ori_dir
-
-# update files for implementing TH2Poly
-copy 128/bin/DQMCollector
-copy 128/bin/visDQMIndex
-copy 128/bin/visDQMRender
-copy 128/lib/python2.7/site-packages/Monitoring/DQM/Accelerator.so
-copy 128/lib/libDQMGUI.so
-
-# replace the include directory
-echo "mv ${dir}/128/include ${dir}/128/tmp"
-mv ${dir}/128/include ${dir}/128/tmp
-copy_dir 128/include
-
-echo ">>> finished!"
+# update rendering plugins for HGCAL DQM plots
+update "workspaces-online.py" "${destination}"
+update "HGCalRenderPlugin.cc" "${destination}/style"
+update "hgcal-layouts.py" "${destination}/layouts"
+update "HGCalAuxiliaryInfo.h" "${destination}/style"
