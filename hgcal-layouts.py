e546981e1b85aba0832075b8ea9c715222a77b42
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2025-09-24 16:46:34 +0200

    improve figure names in the layout folder

--- hgcal-layouts.py
+++ hgcal-layouts.py
@@ -7,19 +7,19 @@ summary = "wafer map for hgcal"
 digis = "digis information"
 
 ################### Links to TOP Summary Histograms #################################
-hgcallayout(dqmitems, "01-layer_1_hex_avgadc",
+hgcallayout(dqmitems, "Layer 1: Average ADC",
           [{ 'path': "HGCAL/EndCap_Minus/Layer_1/Cassette_1/hex_avgadc_layer_1", 'description': quality + hgcallink }])
-hgcallayout(dqmitems, "02-layer_2_hex_avgadc",
+hgcallayout(dqmitems, "Layer 2: Average ADC",
           [{ 'path': "HGCAL/EndCap_Minus/Layer_2/Cassette_1/hex_avgadc_layer_2", 'description': quality + hgcallink }])
-hgcallayout(dqmitems, "03-layer_3_hex_avgadc",
+hgcallayout(dqmitems, "Layer 3: Average ADC",
           [{ 'path': "HGCAL/EndCap_Minus/Layer_3/Cassette_1/hex_avgadc_layer_3", 'description': quality + hgcallink }])
-hgcallayout(dqmitems, "04-layer_4_hex_avgadc",
+hgcallayout(dqmitems, "Layer 4: Average ADC",
           [{ 'path': "HGCAL/EndCap_Minus/Layer_4/Cassette_1/hex_avgadc_layer_4", 'description': quality + hgcallink }])
-hgcallayout(dqmitems, "05-layer_5_hex_avgadc",
+hgcallayout(dqmitems, "Layer 5: Average ADC",
           [{ 'path': "HGCAL/EndCap_Minus/Layer_5/Cassette_1/hex_avgadc_layer_5", 'description': quality + hgcallink }])
-hgcallayout(dqmitems, "06-layer_6_hex_avgadc",
+hgcallayout(dqmitems, "Layer 6: Average ADC",
           [{ 'path': "HGCAL/EndCap_Minus/Layer_6/Cassette_1/hex_avgadc_layer_6", 'description': quality + hgcallink }])
-hgcallayout(dqmitems, "07-layer_7_hex_avgadc",
+hgcallayout(dqmitems, "Layer 7: Average ADC",
           [{ 'path': "HGCAL/EndCap_Minus/Layer_7/Cassette_1/hex_avgadc_layer_7", 'description': quality + hgcallink }])
-hgcallayout(dqmitems, "08-layer_8_hex_avgadc",
+hgcallayout(dqmitems, "Layer 8: Average ADC",
           [{ 'path': "HGCAL/EndCap_Minus/Layer_8/Cassette_1/hex_avgadc_layer_8", 'description': quality + hgcallink }])
